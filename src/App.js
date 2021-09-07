4989b5a9b6cdd6304fa7087794c19cb0e1f36663
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-09-07 11:29:49 -0300

    Unicafe, Step 2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setAll(allClicks + 1)
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allClicks + 0)
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks - 1)
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {allClicks / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
     </div>
   )
 }
